4650572a2c921bdcfc40487863e365296ef93843
hms8905 <hms8905@gmail.com>
2026-02-28 14:03:02 +0900

    no message

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/_global.scss";
 
+const basename = import.meta.env.PROD ? "/portfolio" : "";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/portfolio/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-});
+  },
+}));
